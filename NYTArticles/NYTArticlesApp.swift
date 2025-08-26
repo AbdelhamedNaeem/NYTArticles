9b8313202083da3fd346712d33acee4dfb8eaeb0
Abdelhamid-Naeem <abdelhamid.naeem@floward.com>
2025-08-26 23:36:36 +0300

    Initial Commit

---
+++ NYTArticles/NYTArticlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYTArticlesApp.swift
+//  NYTArticles
+//
+//  Created by Abdelhamid Naeem on 26/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct NYTArticlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
